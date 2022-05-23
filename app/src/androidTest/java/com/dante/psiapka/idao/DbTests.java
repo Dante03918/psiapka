27d43a959c77bc290c208c6e0a3d3571a1792ed5
Dante <dante03918@gmail.com>
2022-05-23 20:56:58 +0200

    DB tests for Dog entity

--- app/src/androidTest/java/com/dante/psiapka/idao/DbTests.java
+++ app/src/androidTest/java/com/dante/psiapka/idao/DbTests.java
@@ -12,14 +12,21 @@ import com.dante.psiapka.model.BreedAndDog;
 import com.dante.psiapka.model.Dog;
 import com.dante.psiapka.model.Gender;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItemInArray;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 @RunWith(AndroidJUnit4.class)
@@ -28,6 +35,9 @@ public class DbTests {
     public Database database;
     public BreedDao breedDao;
     public DogDao dogDao;
+    public Dog testDog;
+    public Breed breedTest;
+
 
     @Before
     public void setUp(){
@@ -35,34 +45,82 @@ public class DbTests {
        database = Room.inMemoryDatabaseBuilder(context, Database.class).build();
        breedDao = database.breedDao();
        dogDao = database.dogDao();
+        testDog = new Dog(1,
+                1,
+                "PedigreeName",
+                "Zosia",
+                Gender.FEMALE,
+                new Date(11111111111L),
+                987654321,
+                "",
+                new Date(222222222L),
+                new Date(2332343524L),
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "");
+        breedTest = new Breed("Chin", null);
 
-       breedDao.insertBreed(new Breed("Chin", null));
-       dogDao.insertDog(new Dog(1,
-               1,
-               "PedigreeName",
-               "Zosia",
-               Gender.FEMALE,
-               new Date(11111111111L),
-               987654321,
-               "",
-               new Date(222222222L),
-               new Date(2332343524L),
-               "",
-               "",
-               "",
-               "",
-               "",
-               "",
-               "",
-               ""));
+        breedDao.insertBreed(breedTest);
+        dogDao.insertDog(testDog);
     }
 
     @Test
     public void getDogsWithBreeds(){
+
         List<BreedAndDog> breedAndDog;
         breedAndDog = breedDao.breedsWithDogsList();
 
-//        assertThat(breedAndDog, hasItem(List<Dog>).);
+        assertFalse(breedAndDog.get(0).dogs.isEmpty());
 
     }
+
+
+    //DogDao tests
+
+    @Test
+    public void dogInsertTest(){
+
+        assertEquals(testDog, dogDao.getDogs().get(0));
+    }
+
+    @Test
+    public void updateDogEntity(){
+        dogDao.updateDog(new Dog(1,
+                1,
+                "PedigreeName",
+                "Frania",
+                Gender.FEMALE,
+                new Date(11111111111L),
+                987654321,
+                "",
+                new Date(222222222L),
+                new Date(2332343524L),
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""));
+        assertEquals("Frania", dogDao.getDogs().stream()
+                .map(el -> el.name)
+                .filter(el -> el.equals("Frania"))
+                .findFirst()
+                .map(el -> el.toString())
+                .orElse(""));
+
+
+    }
+
+    @Test
+    public void deleteDogTest(){
+        assertEquals(1, dogDao.deleteDog(testDog.id));
+    }
+
 }
--- app/src/main/java/com/dante/psiapka/model/Gender.java
+++ app/src/main/java/com/dante/psiapka/model/Gender.java
@@ -1,5 +1,5 @@
 package com.dante.psiapka.model;
 
 public enum Gender {
-    MALE, FEMALE
+    MALE, FEMALE, UNKNOWN
 }
